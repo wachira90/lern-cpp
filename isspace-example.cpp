7e081836a91da6a2041d124033f70f904f87d8ae
Wachira Duangdee <wcrcorp@gmail.com>
2022-02-13 02:58:41 +0700

    Update isspace-example.cpp

--- isspace-example.cpp
+++ isspace-example.cpp
@@ -15,3 +15,12 @@ int main ()
   }
   return 0;
 }
+/*
+For the "C" locale, white-space characters are any of:
+' '	(0x20)	space (SPC)
+'\t'	(0x09)	horizontal tab (TAB)
+'\n'	(0x0a)	newline (LF)
+'\v'	(0x0b)	vertical tab (VT)
+'\f'	(0x0c)	feed (FF)
+'\r'	(0x0d)	carriage return (CR)
+*/
